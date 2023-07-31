c00bf02b46912cb421d0b9cc24f7d2cff372d1e0
Indu-Joshi <indujoshi95@gmail.com>
2023-07-31 15:54:05 +0545

    Hamburger Completed

--- script.js
+++ script.js
@@ -4,21 +4,13 @@ const navItems = document.querySelector(".nav-items");
 const navItem = document.getElementsByClassName("nav-item");
 
 hamburger.addEventListener("click", () => {
-  navItems.style.display = "block";
-  hamburger.style.display = "none";
-  closedHamburger.style.display = "block";
-});
-
-closedHamburger.addEventListener("click", () => {
-  navItems.style.display = "none";
-  closedHamburger.style.display = "none";
-  hamburger.style.display = "block";
+  navItems.classList.toggle("nav-open");
+  hamburger.classList.toggle("fa-xmark");
 });
 
 for (let item of navItem) {
   item.addEventListener("click", () => {
-    navItems.style.display = "none";
-    hamburger.style.display = "block";
-    closedHamburger.style.display = "none";
+    navItems.classList.toggle("nav-open");
+    hamburger.classList.toggle("fa-xmark");
   });
 }
