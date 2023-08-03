bba3478b5ca689fd621dde92d86434cba0755a9c
Indu-Joshi <indujoshi95@gmail.com>
2023-08-03 18:10:33 +0545

    Multiple swipers added

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//----Hamburger-----
 const hamburger = document.querySelector(".hamburger");
 const closedHamburger = document.querySelector(".closed-hamburger");
 const navItems = document.querySelector(".nav-items");
@@ -14,3 +15,41 @@ for (let item of navItem) {
     hamburger.classList.toggle("fa-xmark");
   });
 }
+
+// -- Initialize Swiper --
+var swiper = new Swiper(".mySwiper", {
+  centeredSlides: false,
+  slidesPerView: 3,
+  loop: true,
+  spaceBetween: 20,
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+});
+var swiper2 = new Swiper(".mySwiper2", {
+  centeredSlides: false,
+  slidesPerView: 5,
+  spaceBetween: 10,
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+var swiper3 = new Swiper(".mySwiper3", {
+  spaceBetween: 20,
+  centeredSlides: false,
+  slidesPerView: 4,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
