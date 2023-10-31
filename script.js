8cdcc37093364a23d6e62deae832cf984dd3449f
unknown <indujoshi95@gmail.com>
2023-10-31 16:34:47 +0545

    Correction

--- script.js
+++ script.js
@@ -19,14 +19,25 @@ for (let item of navItem) {
 // -- Initialize Swiper --
 var swiper = new Swiper(".mySwiper", {
   centeredSlides: false,
-  slidesPerView: 3,
-  loop: true,
-  spaceBetween: 20,
   grabCursor: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
     dynamicBullets: true,
+  },
+  breakpoints: {
+    500: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
   },
 });
 var swiper2 = new Swiper(".mySwiper2", {
@@ -37,19 +48,57 @@ var swiper2 = new Swiper(".mySwiper2", {
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
-    dynamicBullets: true,
   },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+  breakpoints: {
+    0: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    500: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 20,
+    },
+  },
 });
 var swiper3 = new Swiper(".mySwiper3", {
-  spaceBetween: 20,
   centeredSlides: false,
-  slidesPerView: 4,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+  },
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 40,
+    },
+    500: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
   },
 });
